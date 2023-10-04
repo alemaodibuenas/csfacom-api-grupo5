8752225067ece9db44f498bc4ae51ea0d8f4511c
Luan Vilela <luan.lopes@ufms.br>
2023-10-03 21:31:37 -0400

    teste aws

--- src/foto/foto.service.ts
+++ src/foto/foto.service.ts
@@ -6,7 +6,7 @@ import { promisify } from 'util';
 import { CreateFotoDto } from './dto/create-foto.dto';
 import { UpdateFotoDto } from './dto/update-foto.dto';
 import { FotoEntity } from './entities/foto.entity';
-import { S3 } from 'aws-sdk';
+// import { S3 } from 'aws-sdk';
 
 @Injectable()
 export class FotoService {
@@ -80,7 +80,7 @@ export class FotoService {
       Key: key,
     };
     try {
-      await s3.deleteObject(params).promise();
+      // await s3.deleteObject(params).promise();
     } catch (e) {
       console.log(e);
     }
@@ -98,18 +98,18 @@ export class FotoService {
     };
 
     //see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
-    const { Location } = await s3.upload(params).promise();
+    // const { Location } = await s3.upload(params).promise();
     file.filename = Location;
     file.key = tipo + key;
     return this.store(file);
   };
 
   getS3() {
-    return new S3({
-      accessKeyId: process.env.BUCKET_ACCESS_KEY_ID,
-      secretAccessKey: process.env.BUCKET_SECRET_ACCESS_KEY,
-      endpoint: process.env.BUCKET_ENDPOINT,
-    });
+    // return new S3({
+    //   accessKeyId: process.env.BUCKET_ACCESS_KEY_ID,
+    //   secretAccessKey: process.env.BUCKET_SECRET_ACCESS_KEY,
+    //   endpoint: process.env.BUCKET_ENDPOINT,
+    // });
   }
 
   getNameFile = (file) => {
